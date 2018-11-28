2dfc2e5ec6707763ec764293ebf24882437b39e7
AmosExe <zxb@ivyb2b.com>
2018-11-28 18:05:06 +0800

    首页

--- project/src/App.vue
+++ project/src/App.vue
@@ -3,13 +3,13 @@
     <div  class="headerWrapper">
         <header  class="header">
           <div class="homeTitle"><img src="https://raw.githubusercontent.com/AmosExe/resource/master/ASite.png"></div>
-            <div  class="container">
+            <div  class="container" >
                
                 <div class="topshow">
-                  <el-row :gutter="0">
-                  <el-col :span="30"><div class="grid-content bg-purple"><el-input v-model="input" placeholder="搜索文档" ></el-input></div></el-col>
-                  <el-col :span="1"><div class="grid-content bg-purple" style="padding-left: 0"><el-button slot="append" icon="el-icon-search"></el-button></div></el-col>
-                  <el-col :span="30"><div>
+                  <el-row :gutter="1">
+                  <el-col :span="60"><div class="grid-content bg-purple" style="width: 500px;"><el-input v-model="input" placeholder="搜索文档" ></el-input></div></el-col>
+                  <el-col :span="500"><div class="grid-content bg-purple" style="float: left;"><el-button slot="append" icon="el-icon-search"></el-button></div></el-col>
+                  <el-col :span="60"><div style="float: left">
                     <ul id="topul">
                       <li style="float: left;width: 50px">1</li>
                       <li  style="float: left;width: 50px">2</li>
@@ -17,19 +17,20 @@
                     </ul>                  
                   </div>
                   </el-col>
-                    <el-col :span="200"><div class="grid-content bg-purple" style="padding-left: 20px">
+                    <el-col :span="60"><div class="grid-content bg-purple" style="padding-left: 20px">
                       <el-dropdown @command="handleCommand">
-                    <span class="el-dropdown-link" style="font-size: 22px">
+                    <span class="el-dropdown-link" style="font-size: 22px;float: left;">
                       功 能<i class="el-icon-arrow-down el-icon--right"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown" style="width: 140px">
-                      <el-dropdown-item command="a" style="width: 100px">黄金糕</el-dropdown-item>
-                      <el-dropdown-item command="b"  style="width: 100px">狮子头</el-dropdown-item>
+                      <el-dropdown-item command="a" style="width: 100px">写文章</el-dropdown-item>
+                      <el-dropdown-item command="b"  style="width: 100px">博客</el-dropdown-item>
                       <el-dropdown-item command="c"  style="width: 100px" >螺蛳粉</el-dropdown-item>
                     </el-dropdown-menu>
                   </el-dropdown>
                     </div></el-col>
-                  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+                  <el-col :span="2"><div class="grid-content bg-purple"><el-button type="warning" plain >登录</el-button></div></el-col>
+                   <el-col :span="1"><div class="grid-content bg-purple"><el-button type="warning" plain >注册</el-button></div></el-col>
                   </el-row>
              </div>
             </div>
---
+++ project/src/components/top.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<p>
+			你好,我是自定义组件
+		</p>
+	</div>
+</template>
